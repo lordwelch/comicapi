6798cd2a83aa247417724ac17e866a4d2cf91d1d
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2021-01-17 10:30:00 +0100

    Update version and classifier information

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 setup(
         name = 'comicapi',
-        version = '2.1.2',
+        version = '2.1.3',
         description = 'Comic archive (cbr/cbz/cbt) and metadata utilities. Extracted from the comictagger project.',
         author = 'Iris W',
+        maintainer = "@OzzieIsaacs",
         packages = ['comicapi'],
         install_requires = ['natsort>=3.5.2'],
         extras_require = {
@@ -11,5 +12,9 @@ setup(
         },
         python_requires = '>=2.7.0',
         url = 'https://github.com/OzzieIsaacs/comicapi',
-        classifiers = ['License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)']
+        classifiers = [
+                "Programming Language :: Python :: 3",
+                'License :: OSI Approved :: Apache Software License',
+                "Operating System :: OS Independent",
+        ]
 )
